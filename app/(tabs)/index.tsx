0a67104f2724e0c0650b40b2de18af8a2bedf321
macethewindu66 <madduxberry31@gmail.com>
2026-01-10 13:03:43 -0800

    Now loading full chapters. Made verses separate and clean, with small verse numbers denoting new verses.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,33 @@
 import { useFocusEffect } from 'expo-router';
 import { useSQLiteContext } from 'expo-sqlite';
 import { useState } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, ScrollView } from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import  React  from 'react';
+
+type VerseRow = {
+    osis: string;
+    chapter: number;
+    verse: number;
+    text: string;
+};
 
 export default function Tab() {
 
-    const [data, setData] = useState([]);
+    const [data, setData] = useState<VerseRow[]>([]);
+    const [chapter, setChapter] = useState(1);
 
     const database = useSQLiteContext();
 
     const loadData = async() =>{
-        const result = await database.getAllAsync(
+        const result = await database.getAllAsync<VerseRow>(
             `SELECT b.osis, v.chapter, v.verse, v.text
             FROM verses v
             JOIN books b ON b.id = v.book_id
-            WHERE b.osis = ? AND v.chapter = ? AND v.verse = ?`,
-            ['JHN', 1, 1]
+            WHERE b.osis = ? AND v.chapter = ?`,
+            ['MRK', chapter]
         );
+        console.log(result)
         setData(result);
     };
 
@@ -29,11 +39,22 @@ export default function Tab() {
 
 
     return (
-        <View style={styles.container}>
-            <View>
-                <Text>{data?.[0]?.text ?? 'Loading...'}</Text>
-            </View>
-        </View>
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.chapter_container}>
+                    <ScrollView style={styles.chapter_container}>
+                        <Text style={styles.verse_text}>
+                            {data.map((v) => (
+                            <Text key={`${v.osis}:${v.chapter}:${v.verse}`}>
+                                <Text style={styles.verseNumber}>{v.verse} </Text>
+                                {v.text + ' '}
+                            </Text>
+                            ))}
+                        </Text>
+                    </ScrollView>
+                </View>
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
 
@@ -44,4 +65,18 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    chapter_container: {
+        flex: 1,
+        padding: 10,
+    },
+    verse_text: {
+        fontSize: 16,
+        lineHeight: 30,
+    },
+
+    verseNumber: {
+        fontWeight: '600',
+        fontSize: 12,
+    },
+
 });
